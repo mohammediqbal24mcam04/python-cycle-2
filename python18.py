d824b3eb1135286a7ae64c63c547c50b68bdeb6a
mohammed iqbal v <24mcam04@meaec.edu.in>
2024-10-14 12:53:58 +0530

    Update python18.py
    Commit

--- python18.py
+++ python18.py
@@ -1,4 +1,16 @@
-dict1 = {'a': 1, 'b': 2}
-dict2 = {'b': 3, 'c': 4}
+
+dict1_input = input("Enter first dictionary (key:value pairs separated by commas): ")
+dict1 = dict(item.split(":") for item in dict1_input.split(",") if item)
+
+
+dict2_input = input("Enter second dictionary (key:value pairs separated by commas): ")
+dict2 = dict(item.split(":") for item in dict2_input.split(",") if item)
+
+
+dict1 = {k: int(v) for k, v in dict1.items()}
+dict2 = {k: int(v) for k, v in dict2.items()}
+
+
 merged_dict = {**dict1, **dict2}
-print(merged_dict)
+
+print("Merged Dictionary:", merged_dict)
